855c2f2c6da8a690e14d8bef4b58e38ee90a5b8c
J. Cohen <hostelmarketing@gmail.com>
2013-11-15 15:09:52 -0800

    Works

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from polls import views
 
 urlpatterns = patterns('',
         url(r'^$', views.IndexView.as_view(), name='index'),
-        url(r'^(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
-        url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+        url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+        url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
         url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
